1ee9a6f755ab8be12b651506733c81e7b47f222a
lab-109-inf-15 <nm23-0185@unphu.edu.do>
2024-01-24 21:06:09 -0400

    Creacion estructura de App.

---
+++ src/SimpleAgenda.java
@@ -0,0 +1,19 @@
+public class SimpleAgenda {
+    public static void agregarContacto (String nombre) {
+
+    }
+    public static void removerContacto (int id) {
+
+    }
+    public static String actualizarContacto (int id) {
+return "N/A";
+    }
+    public static void mostrarContactos () {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+
